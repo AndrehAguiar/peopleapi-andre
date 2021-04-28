a45416597003c6c2c94b40f80ab62400d80cacd1
AndrehAguiar <andreaguiar.g@gmail.com>
2021-04-27 22:06:33 -0300

    First commit

---
+++ src/main/java/com/digitalinnovation/CadastroPessoas/CadastroPessoasApplication.java
@@ -0,0 +1,15 @@
+package com.digitalinnovation.CadastroPessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CadastroPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/CadastroPessoas/CadastroPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.CadastroPessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
