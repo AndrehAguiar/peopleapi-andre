ac2294f2ed0c31731b59885e282725f2c6e98eea
AndrehAguiar <andreaguiar.g@gmail.com>
2021-04-28 00:30:59 -0300

    Add .jar

--- src/main/java/com/digitalinnovation/CadastroPessoas/CadastroPessoasApplication.java
+++ src/main/java/com/digitalinnovation/CadastroPessoas/CadastroPessoasApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 public class CadastroPessoasApplication {
 
 	public static void main(String[] args) {
